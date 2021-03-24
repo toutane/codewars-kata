641f81284bf1b9640e4491c0919d5ad9c1d27198
Charles-Antoine LEGER <ca@leger.email>
2021-03-24 20:12:37 +0100

    Add Dominant array element 7 kyu kata

---
+++ 7kyu/dominant-array-elements.js
@@ -0,0 +1,9 @@
+function solve(arr) {
+  const res = []
+  arr.map((x, i, arr) => {
+    if (!arr.slice(i+1).some(q => q>=x)) {res.push(x)
+    console.log(res)
+    }       
+  })
+  return res
+  };
