410cec16668d9bf66d8f53ed00d0c19a2f3e18d0
Charles-Antoine LEGER <ca@leger.email>
2021-04-19 16:10:50 +0200

    Add 7kyu go Mumbling kata

--- go/main.go
+++ go/main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/toutane/codewars-kata/go/8kyu"
-	"github.com/toutane/codewars-kata/go/7kyu"
+
+	kata7 "github.com/toutane/codewars-kata/go/7kyu"
+	kata8 "github.com/toutane/codewars-kata/go/8kyu"
 )
 
 func main() {
-	fmt.Println(kata8.MakeUpperCase("hello, world"))
-	// Expected : "HELLO, WORLD"
+	kata8.MakeUpperCase("hello, world")
+	// Expected: "HELLO, WORLD"
 
 	x := []float64{0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25}
-	fmt.Println(kata7.Gps(15, x))
-	// Expected : 74 (max speed "floor")
+	kata7.Gps(15, x)
+	// Expected: 74 (max speed "floor")
+
+	str := "ZpglnRxqenU"
+	fmt.Println(kata7.Accum(str))
+	// Expected: "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
 }
